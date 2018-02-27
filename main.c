b48f3e9baaabb4108c63c83e0fe00b4843eacd15
Garth Webb <garth.webb@gmail.com>
2018-02-26 21:13:36 -0800

    Speed detection and switch monitoring (test) working

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <avr/io.h>
 #include "main.h"
 #include <avr/interrupt.h>
+#include <stdlib.h>
 
 // define baud
 #define BAUD 38400L
@@ -18,22 +19,44 @@
 #define MOTOR_CTRL_ON PORTC |= _BV(MOTOR_CTRL)
 #define MOTOR_CTRL_OFF PORTC &= ~_BV(MOTOR_CTRL)
 
-#define AUTO_RUN PORTD2
+#define AUTO_RUN PIND2
 #define AUTO_RUN_DD DD2
+#define AUTO_RUN_INT PCINT18
 
-#define MANUAL_RUN PORTD3
+#define MANUAL_RUN PIND3
 #define MANUAL_RUN_DD DD3
+#define MANUAL_RUN_INT PCINT19
 
-#define MOTOR_PULSE PORTD4
+#define MOTOR_PULSE PIND4
 #define MOTOR_PULSE_DD DD4
 
+// Because we sample every 10ms, this is Frequency/10
+#define MIN_FREE_FREQUENCY 188
+
+#define RUN_MODE_FREE 0
+#define RUN_MODE_LOADED 1
+
+// Either 1 or 0 depending on whether the current measured input is high or low
+uint8_t pulse = 0;
+// A counter that tracks high to low transitions
+long counter = 0;
+
+uint8_t run_mode = RUN_MODE_FREE;
+
 char msg_buf[80];
-int last = 0;
+long last = 0;
+
+// Flags to raise in the interrupt whenever the manual or auto switch was thrown
+uint8_t manual_run_flag = 0;
+uint8_t auto_run_flag = 0;
+uint16_t debounce_counter = 0;
 
 void init_avr() {
     init_ports();
-    init_counter();
-    init_timer();
+//    init_counter();
+    init_counter_timer();
+    init_frequency_timer();
+    init_pin_interrupts();
 
     sei();
 }
@@ -54,16 +77,32 @@ void init_ports() {
     PORTD |= _BV(AUTO_RUN_DD) | _BV(MANUAL_RUN_DD) | _BV(MOTOR_PULSE_DD);
 }
 
+void init_pin_interrupts() {
+    PCMSK2 |= _BV(AUTO_RUN_INT) | _BV(MANUAL_RUN_INT);
+    PCICR |= (1<<PCIE2);
+}
+
 void init_counter() {
     // External clock source on T0 pin. Clock on rising edge.
     TCCR0B |= (1 << CS02) | (1 << CS01) | (1 << CS00);
 }
 
-void init_timer() {
+void init_counter_timer() {
+    // Enable Interrupt TimerCounter0 Compare Match A (TIMER0_COMPA_vect)
+    TIMSK0 = _BV(OCIE0A);
+    // Mode = CTC
+    TCCR0A = _BV(WGM01);
+    // 1/(Clock/Scaling) == 1/(8_000_000/64) == 0.000008 s == 8 us per tick
+    TCCR0B = _BV(CS01) | _BV(CS00);
+    // 0.000008*5 == 0.00004 == 40us TIMER0_COMPA_vect will be triggered 25000 times per second.
+    OCR0A = 5;
+}
+
+void init_frequency_timer() {
     // Enable Interrupt Timer/Counter1, Output Compare A (TIMER1_COMPA_vect)
     TIMSK1 = _BV(OCIE1A);
 
-    // Clock/1024, 0.000128 seconds per tick, Mode=CTC
+    // 1/(Clock/Scaling) == 1/(8_000_000/1024) == 0.000128 seconds per tick, Mode=CTC
     TCCR1B = _BV(CS12) | _BV(CS10) | _BV(WGM12);
 
     // 0.000128*781 = 0.01 s TIMER1_COMPA_vect will be triggered every 10ms
@@ -71,12 +110,41 @@ void init_timer() {
     OCR1AL = 781 & 0xFF;
 }
 
+ISR(TIMER0_COMPA_vect)
+{
+    if (PIND & _BV(MOTOR_PULSE)) {
+        if (!pulse) {
+            pulse = 1;
+        }
+    } else {
+        if (pulse) {
+            pulse = 0;
+            counter++;
+        }
+    }
+}
+
 ISR(TIMER1_COMPA_vect) 
 {
-    last = TCNT0;
-    TCNT0 = 0;
-    sprintf(msg_buf, "%d\n", last);
-    send(msg_buf);
+    //last = TCNT0;
+    //TCNT0 = 0;
+    last = counter;
+    counter = 0;
+    // sprintf(msg_buf, "%ld\n", last);
+    // send(msg_buf);
+}
+
+ISR(PCINT2_vect)
+{
+    if (debounce_counter == 0) {
+        // Pins are active low
+        if ((PIND & _BV(AUTO_RUN)) == 0) {
+            auto_run_flag = 1;
+        }
+        if ((PIND & _BV(MANUAL_RUN)) == 0) {
+            manual_run_flag = 1;
+        }
+    }
 }
 
 /**
@@ -106,6 +174,46 @@ int main() {
     init_avr();
     init_uart();
 
+    send("-- Initialized\n");
+
+    send("DDRD: ");
+    itoa(DDRD, msg_buf, 2);
+    send(msg_buf);
+    send("\n");
+
+    send("PORTD: ");
+    itoa(PORTD, msg_buf, 2);
+    send(msg_buf);
+    send("\n");
+
     while (1) {
+        if (last >= MIN_FREE_FREQUENCY) {
+            if (run_mode == RUN_MODE_LOADED) {
+                sprintf(msg_buf, "Free running -- %ld\n", last);
+                send(msg_buf);
+                run_mode = RUN_MODE_FREE;
+            }
+        } else {
+            if (last != 0 && run_mode == RUN_MODE_FREE) {
+                sprintf(msg_buf, "Under load -- %ld\n", last);
+                send(msg_buf);
+                run_mode = RUN_MODE_LOADED;
+            }
+        }
+
+        if (manual_run_flag) {
+            send("== Detected Manual Request ==\n");
+            debounce_counter = 40960;
+            manual_run_flag = 0;
+        }
+        if (auto_run_flag) {
+            send("== Detected Auto Request ==\n");
+            debounce_counter = 40960;
+            auto_run_flag = 0;
+        }
+
+        if (debounce_counter > 0) {
+            debounce_counter--;
+        }
     }
 }
--- main.h
+++ main.h
@@ -4,7 +4,9 @@
 void init_avr(void);
 void init_ports(void);
 void init_counter(void);
-void init_timer(void);
+void init_pin_interrupts(void);
+void init_counter_timer(void);
+void init_frequency_timer(void);
 void init_uart(void);
 
 void send(const char *string);
